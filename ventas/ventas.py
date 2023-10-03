17ea3d2a5e81ba4a5a63e86d9d026e4b6fbd67b1
Michael Gamarra <76668684+MichaelGamarra@users.noreply.github.com>
2023-10-02 23:35:07 -0300

    Modificacion 3/10/2023

--- ventas/ventas.py
+++ ventas/ventas.py
@@ -6,7 +6,7 @@ from kivy.properties import BooleanProperty
 from kivy.uix.recycleboxlayout import RecycleBoxLayout
 from kivy.uix.behaviors import FocusBehavior
 from kivy.uix.recycleview.layout import LayoutSelectionBehavior
-
+from kivy.uix.popup import Popup
 
 
 
@@ -55,14 +55,55 @@ class SelectableBoxLayout(RecycleDataViewBehavior, BoxLayout):
         if self.collide_point(*touch.pos) and self.selectable:
             return self.parent.select_with_touch(self.index, touch)
 
+    def apply_selection(self, rv, index, is_selected):
+        ''' Respond to the selection of items in the view. '''
+        self.selected = is_selected
+        if is_selected:
+            rv.data[index]['seleccionado']= True
+        else:
+            rv.data[index]['seleccionado']= False
+
+
+
+
+
+
+class SelectableBoxLayoutPopup(RecycleDataViewBehavior, BoxLayout):
+    ''' Add selection support to the Label '''
+    index = None
+    selected = BooleanProperty(False)
+    selectable = BooleanProperty(True)
+
+    def refresh_view_attrs(self, rv, index, data):
+        self.index = index
+        self.ids['_codigo'].text = data['codigo']
+        self.ids['_articulo'].text = data['nombre'].capitalize()
+        self.ids['_cantidad'].text = str(data['cantidad'])
+        self.ids['_precio'].text = str("{:.2f}".format(data['precio']))
+        return super(SelectableBoxLayoutPopup, self).refresh_view_attrs(
+            rv, index, data)
+
+
+
+    def on_touch_down(self, touch):
+        ''' Add selection on touch down '''
+        if super(SelectableBoxLayoutPopup, self).on_touch_down(touch):
+            return True
+        if self.collide_point(*touch.pos) and self.selectable:
+            return self.parent.select_with_touch(self.index, touch)
+
+
+
 
     def apply_selection(self, rv, index, is_selected):
         ''' Respond to the selection of items in the view. '''
         self.selected = is_selected
         if is_selected:
-            print("selection changed to {0} y {1}".format(rv.data[index], 22))
+            rv.data[index]['seleccionado']= True
         else:
-            print("selection removed for {0}".format(rv.data[index]))
+            rv.data[index]['seleccionado']= False
+
+
 
 
 
@@ -70,6 +111,7 @@ class RV(RecycleView):
     def __init__(self, **kwargs):
         super(RV, self).__init__(**kwargs)
         self.data = []
+        self.modificar_producto= None
     
     def agregar_articulo(self,articulo):
         articulo['seleccionado'] = False
@@ -87,7 +129,96 @@ class RV(RecycleView):
         else:
             self.data.append(articulo)
 
+    def eliminar_articulo(self):
+        indice=self.articulo_seleccionado()
+        precio=0
+        if indice >=0:
+            self._layout_manager.deselect_node(self._layout_manager._last_selected_node)
+            precio= self.data[indice]['precio_total']
+            self.data.pop(indice)
+            self.refresh_from_data()
+        return precio
 
+    def articulo_seleccionado(self):
+        indice = -1
+        for i in range(len(self.data)):
+            if self.data[i]['seleccionado']:
+                indice = i
+                break
+        return indice
+
+    def modificar_articulo(self):
+        indice=self.articulo_seleccionado()
+        if indice >=0:
+            popup=CambiarCantidadPopup(self.data[indice],self.actualizar_articulo)
+            popup.open()
+
+
+    def actualizar_articulo(self,valor):
+        indice= self.articulo_seleccionado()
+        if indice >=0:
+            if valor==0:
+                self.data.pop(indice)
+                self._layout_manager.deselect_node(self._layout_manager._last_selected_node)
+            else:
+                self.data[indice]['cantidad_carrito']=valor
+                self.data[indice]['precio_total']=self.data[indice]['precio']*valor
+            self.refresh_from_data()
+            nuevo_total= 0
+            for data in self.data:
+                nuevo_total+= data['precio_total']
+            self.modificar_producto(False, nuevo_total)
+
+
+class CambiarCantidadPopup(Popup):
+    def __init__(self, data, actualizar_articulo_callback,**kwargs):
+        super(CambiarCantidadPopup, self).__init__(**kwargs)
+        self.data = data
+        self.actualizar_articulo=actualizar_articulo_callback
+        self.ids.info_nueva_cantidad_1.text = "Producto: " + self.data['nombre'].capitalize()
+        self.ids.info_nueva_cantidad_2.text = "Cantidad: " + str(self.data['cantidad_carrito'])
+    
+
+    def validar_input(self,texto_input):
+        try:
+            nueva_cantidad = int(texto_input)
+            self.ids.notificacion_no_valido.text= ' '
+            self.actualizar_articulo(nueva_cantidad)
+            self.dismiss()
+        except:
+            self.ids.notificacion_no_valido.text= 'Cantidad no Valida'
+
+
+
+class ProductoPorNombrePopup(Popup):
+    def __init__(self,input_nombre, agregar_producto_callback,**kwargs):
+        super(ProductoPorNombrePopup, self).__init__(**kwargs)
+        self.input_nombre = input_nombre
+        self.agregar_producto = agregar_producto_callback
+    
+
+
+    def mostrar_articulos(self):
+        self.open()
+        for nombre in inventario:
+            if nombre['nombre'].lower().find(self.input_nombre)>=0:
+                producto = {'codigo':nombre['codigo'], 'nombre': nombre['nombre'], 'precio': nombre['precio'],'cantidad': nombre['cantidad']}
+                self.ids.rvs.agregar_articulo(producto)
+
+    def seleccionar_articulo(self):
+        indice=self.ids.rvs.articulo_seleccionado()
+        if indice >= 0:
+            _articulo=self.ids.rvs.data[indice]
+            articulo={}
+            articulo['codigo']= _articulo['codigo']
+            articulo['nombre']= _articulo['nombre']
+            articulo['precio']= _articulo['precio']
+            articulo['cantidad_carrito']= 1
+            articulo['cantidad_inventario']= _articulo['cantidad']
+            articulo['precio_total']= _articulo['precio']
+            if callable(self.agregar_producto):
+                self.agregar_producto(articulo)
+            self.dismiss()
 
 
 
@@ -96,6 +227,7 @@ class VentasWindow(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.total= 0.0
+        self.ids.rvs.modificar_producto = self.modificar_producto
 
     def admin(self):
         print("Admin Presionado")
@@ -104,10 +236,17 @@ class VentasWindow(BoxLayout):
         print("signout Presionado")
 
     def eliminar_producto(self):
-        print("Eliminar_Producto Seleccionado")
+        menos_precio=self.ids.rvs.eliminar_articulo()
+        self.total-=menos_precio
+        self.ids.sub_total.text ='$ '+"{:.2f}".format(self.total)
 
-    def modificar_producto(self):
-        print("Modificar_Producto Seleccionado")
+    def modificar_producto(self, cambio= True, nuevo_total= None):
+        if cambio:
+            self.ids.rvs.modificar_articulo()
+        else:
+            self.total=nuevo_total
+            self.ids.sub_total.text='$ '+"{:.2f}".format(self.total)
+
 
     def agregar_producto_codigo(self, codigo):
         for producto in inventario:
@@ -131,7 +270,9 @@ class VentasWindow(BoxLayout):
 
 
     def agregar_producto_nombre(self, nombre):
-        print("Se mando ", nombre)
+        self.ids.buscar_nombre.text=' '
+        popup=ProductoPorNombrePopup(nombre,self.agregar_producto)
+        popup.mostrar_articulos()
 
     def pagar(self):
         print ("Pagar")
